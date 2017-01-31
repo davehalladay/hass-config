be090b49f5b2f57f266a02ab57989baf261aa68f
David Halladay <david@davidhalladay.com>
2017-01-30 22:31:54 -0500

    clean up repo and add new office365 sync script

---
+++ office_365_sync.py
@@ -0,0 +1,26 @@
+from O365 import Schedule
+import sqlite3
+from time import mktime
+from datetime import datetime
+from os import environ
+
+db_name = "office_365_cal.db"
+
+conn = sqlite3.connect(db_name)
+
+user_name = environ.get("OFFICE_USER_NAME")
+password = environ.get("OFFICE_PASSWORD")
+
+schedule = Schedule((user_name, password))
+cal = schedule.getCalendars()
+for calendar in schedule.calendars:
+    event = calendar.getEvents()
+    for event in calendar.events:
+        try:
+            with conn:
+                subject = event.getSubject()
+                start_time = datetime.fromtimestamp(mktime(event.getStart()))
+                end_time = datetime.fromtimestamp(mktime(event.getEnd()))
+                conn.execute('insert into cal values(?, ?, ?)', (subject, start_time, end_time))
+        except sqlite3.IntegrityError:
+            pass
